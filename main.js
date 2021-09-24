6803a25751a5d746630b721e5d075b70100774b9
Jao <joaovictorjgar@gmail.com>
2021-09-24 12:16:48 -0300

    testes

---
+++ main.js
@@ -0,0 +1,12 @@
+var nome = "Rafael Galleani";
+var idade = 29;
+var frase = "O Kaique namora a talita";
+//alert("bem vinde " + nome +"você tem "+idade+"anos");
+console.log(frase.replace("Kaique", "Carlos"));
+console.log(frase.toUpperCase("Kaique", "Carlos"));
+var lista = ["maça","pera","laranja"];
+console.log(lista[1]);
+// lista.push("uva");
+// lista.pop();
+
+console.log(lista)
